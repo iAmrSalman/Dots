8149dc9b081e2666856ddf082a021bd73be57b81
Amr Salman <iamrsalman@gmail.com>
2017-08-20 23:28:08 +0200

    added offline error and localizedDescription for DotsError

--- Dots/Classes/DotsError.swift
+++ Dots/Classes/DotsError.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum DotsError: Error {
+public enum DotsError: Error {
   public enum ParameterEncodingFailureReason {
     case missingURL
     case jsonEncodingFailed(error: Error)
@@ -16,5 +16,29 @@ enum DotsError: Error {
   }
   
   case parameterEncodingFailed(reason: ParameterEncodingFailureReason)
-  
+  case offline
+}
+
+extension DotsError: LocalizedError {
+  public var errorDescription: String? {
+    switch self {
+    case .offline:
+      return "No Internet access"
+    case .parameterEncodingFailed(let reason):
+      return reason.localizedDescription
+    }
+  }
+}
+
+extension DotsError.ParameterEncodingFailureReason {
+  var localizedDescription: String {
+    switch self {
+    case .missingURL:
+      return "URL request to encode was missing a URL"
+    case .jsonEncodingFailed(let error):
+      return "JSON could not be encoded because of error:\n\(error.localizedDescription)"
+    case .propertyListEncodingFailed(let error):
+      return "PropertyList could not be encoded because of error:\n\(error.localizedDescription)"
+    }
+  }
 }
